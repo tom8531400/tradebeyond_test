2abcbf1f549f4c0e235cf901424df70518d0f163
tom8531400 <tom8531400@yahoo.com.tw>
2026-01-29 22:11:56 +0800

    Initial commit

---
+++ src/main/java/com/tradebeyond/backend/TradebeyondBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tradebeyond.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradebeyondBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradebeyondBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tradebeyond/backend/TradebeyondBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradebeyond.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradebeyondBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
